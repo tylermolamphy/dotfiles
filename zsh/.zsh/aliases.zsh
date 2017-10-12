ceb440e55d186b4f374242f57bdf536c5af1f263
Tyler Molamphy <tyler@molamphy.net>
2017-10-12 11:22:03 -0600

    adjust aliases

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -6,7 +6,7 @@
 #   ██    ░░░░░██░██  ░██ 
 #  ██████ ██████ ░██  ░██ 
 # ░░░░░░ ░░░░░░  ░░   ░░  
-#
+
 #  ▓▓▓▓▓▓▓▓▓▓
 # ░▓ author ▓ xero <x@xero.nu>
 # ░▓ code   ▓ http://code.xero.nu/dotfiles
@@ -15,23 +15,18 @@
 # ░░░░░░░░░░
 #
 #█▓▒░ aliases
-alias xyzzy="echo nothing happens"
 alias ls="ls -hF --color=auto"
 alias ll="ls -lahF --color=auto"
 alias lsl="ls -lhF --color=auto"
 alias "cd.."="cd ../"
 alias up="cd ../"
-alias rmrf="rm -rf"
 alias psef="ps -ef"
-alias mkdir="mkdir -p"
-alias cp="cp -r"
-alias scp="scp -r"
 alias xsel="xsel -b"
 alias fuck='sudo $(fc -ln -1)'
 alias v="vim"
 alias vi="vim"
 alias emacs="vim"
-alias g="hub"
+alias g="git"
 alias ga="git add"
 alias gc="git commit -m"
 alias gs="git status"
@@ -40,50 +35,19 @@ alias gf="git fetch"
 alias gm="git merge"
 alias gr="git rebase"
 alias gp="git push"
-alias gu="git unstage"
-alias gg="git graph"
+alias gu="git pull"
+alias gg="git add && git commit && git push"
 alias gco="git checkout"
-alias gcs="git commit -S -m"
-alias gpr="hub pull-request"
 alias ag="ag --color --color-line-number '0;35' --color-match '46;30' --color-path '4;36'"
 alias tree='tree -CAFa -I "CVS|*.*.package|.svn|.git|.hg|node_modules|bower_components" --dirsfirst'
 alias rock="ncmpcpp"
 alias mixer="alsamixer"
-alias genplaylist="cd ~/music;find . -name '*.mp3' -o -name '*.flac'|sed -e 's%^./%%g' > ~/.mpd/playlists/all.m3u"
 alias matrix="cmatrix -b"
 alias tempwatch="while :; do sensors; sleep 1 && clear; done;"
-alias term="urxvtc -hold -e " #used for run menu
-alias sprunge="curl -F 'sprunge=<-' http://sprunge.us"
-alias clbin="curl -F 'clbin=<-' https://clbin.com"
-alias toiletlist='for i in ${TOILET_FONT_PATH:=/usr/share/figlet}/*.{t,f}lf; do j=${i##*/}; echo ""; echo "╓───── "$j; echo "╙────────────────────────────────────── ─ ─ "; echo ""; toilet -d "${i%/*}" -f "$j" "${j%.*}"; done'
-alias ascii="toilet -t -f 3d"
-alias future="toilet -t -f future"
-alias rusto="toilet -t -f rusto"
-alias rustofat="toilet -t -f rustofat"
-alias lol="base64 </dev/urandom | lolcat"
-alias pacman="sudo pacman"
-alias update="pacman-colors && yaourt -Syua"
+alias upcp="sudo apt update && sudo apt dist-upgrade"
 alias systemctl="sudo systemctl"
-alias :q="sudo systemctl poweroff"
-alias ZZ="quit"
-alias disks='echo "╓───── m o u n t . p o i n t s"; echo "╙────────────────────────────────────── ─ ─ "; lsblk -a; echo ""; echo "╓───── d i s k . u s a g e"; echo "╙────────────────────────────────────── ─ ─ "; df -h;'
 alias todo="bash ~/code/sys/todo"
 alias record="ffmpeg -f x11grab -s 1366x768 -an -r 16 -loglevel quiet -i :0.0 -b:v 5M -y" #pass a filename
-alias nexus="jmtpfs ~/nexus"
-
-#█▓▒░ dumb tmux trix
-alias tsad="printf '\033k┐(T_T)┌\033\\'"
-alias tshrug="printf '\033k┐(\`-\`)┌\033\\'"
-alias tlol="printf '\033k┐(^0^)┌\033\\'"
-
-#█▓▒░ update mpd database
-function genplaylist() {
-  cd ~/music
-  find . -name '*.mp3' -o -name '*.flac'|sed -e 's%^./%%g' > ~/.config/mpd/playlists/all.m3u
-  mpc clear
-  mpc load all.m3u
-  mpc update
-}
 
 function t() {
   X=$#
