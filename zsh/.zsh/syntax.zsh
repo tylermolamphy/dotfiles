ca74424685db1ca65816331caa71e5c1f5a67f08
Tyler Molamphy <tyler@molamphy.net>
2017-10-13 17:10:16 -0600

    add zsh plugin as subtree

--- zsh/.zsh/syntax.zsh
+++ zsh/.zsh/syntax.zsh
@@ -15,7 +15,8 @@
 # ░░░░░░░░░░
 #
 #█▓▒░ source the plugin https://github.com/zsh-users/zsh-syntax-highlighting
-source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+#source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/dotfiles/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 #█▓▒░ color overrides
 ZSH_HIGHLIGHT_STYLES[default]='none'
