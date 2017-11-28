3e6523efb72983e0dc901e5008e86db0b5a102a7
Tyler Molamphy <tyler@molamphy.net>
2017-11-28 22:54:30 +0000

    add mosh

--- scripts/scripts/install-base.sh
+++ scripts/scripts/install-base.sh
@@ -3,11 +3,11 @@ cat /etc/os-release
 if [ -n "$(command -v apt)" ]; then
     sudo apt update
     sudo apt dist-upgrade -y
-    sudo apt install stow zsh tmux keychain wget iftop -y
+    sudo apt install stow zsh tmux keychain wget iftop mosh -y
     exit 0
 fi
 if [ -n "$(command -v yum)" ]; then
     sudo yum update -y
-    sudo yum install stow zsh tmux keychain curl wget iftop -y
+    sudo yum install stow zsh tmux keychain curl wget iftop mosh -y
     exit 0
 fi
