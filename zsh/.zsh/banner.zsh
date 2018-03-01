e4f61c5391fed6c408e961fb9fc69622d25a7f12
Tyler Molamphy <tyler@molamphy.net>
2018-02-28 18:18:23 -0700

    pretty on macos

--- zsh/.zsh/banner.zsh
+++ zsh/.zsh/banner.zsh
@@ -1,2 +1,5 @@
-
+if [ -n "$(uname | grep Darwin)" ]; then
+echo $(whoami)@$(hostname | sed s/.local//g) running macos $(sw_vers -productVersion)
+else
 echo $(whoami)@$(hostname) running $(grep PRETTY_NAME /etc/os-release | cut -f 2 -d \")
+fi
