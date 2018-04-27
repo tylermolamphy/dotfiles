f0cafe35fc90f975ce9659e87e6b5788e620f215
Tyler Molamphy <tyler@molamphy.net>
2018-04-27 12:58:44 -0600

    add junegunn/vim-easy-align

--- vim/.vim/rake.sh
+++ vim/.vim/rake.sh
@@ -1,4 +1,4 @@
-plugins="vimwiki/vimwiki.git tpope/vim-markdown.git scrooloose/nerdtree.git junegunn/fzf.git nathanaelkane/vim-indent-guides.git"
+plugins="vimwiki/vimwiki.git tpope/vim-markdown.git scrooloose/nerdtree.git junegunn/vim-easy-align.git junegunn/fzf.git nathanaelkane/vim-indent-guides.git"
 rm -rf ~/.vim/bundle/*
 for plugin in $plugins
 do git clone https://github.com/$plugin ~/.vim/bundle/`echo $plugin | cut -d / -f 2`
