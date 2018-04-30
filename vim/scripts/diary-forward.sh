c4972f7218ca2134fea8fd9feb321dc2bcb1d73a
Tyler Molamphy <tyler@molamphy.net>
2018-04-30 17:04:08 -0600

    protect me from myself

--- vim/scripts/diary-forward.sh
+++ vim/scripts/diary-forward.sh
@@ -1,2 +1,8 @@
-cd ~/vimwiki/diary && cat $(ls -t *.md | head -1) | grep '\[*\]' | grep -v "\[X\]" > $(date +%Y-%m-%d.md)
+today=$(date +%Y-%m-%d.md)
+if [ -f ~/vimwiki/diary/$today ]; then
+echo Nope
+exit 1
+fi
+cd ~/vimwiki/diary && cat $(ls -t *.md | head -1) | grep '\[*\]' | grep -v "\[X\]" > ~/vimwiki/diary/$file
 echo Done
+exit 0
