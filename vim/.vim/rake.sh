0f0d52bdae2e9ddb55cbf1519723293a375555d8
Tyler Molamphy <tyler@molamphy.net>
2018-04-30 20:05:22 -0600

    add gruvbox and airline

--- vim/.vim/rake.sh
+++ vim/.vim/rake.sh
@@ -5,7 +5,9 @@ scrooloose/nerdtree.git \
 junegunn/vim-easy-align.git \
 junegunn/fzf.git \
 nathanaelkane/vim-indent-guides.git \
-csexton/trailertrash.vim.git"
+csexton/trailertrash.vim.git \
+vim-airline/vim-airline.git \
+morhetz/gruvbox.git"
 rm -rf ~/.vim/bundle/*
 for plugin in $plugins
 do git clone https://github.com/$plugin ~/.vim/bundle/`echo $plugin | cut -d / -f 2`
