6f26a2f4f3b05fedf6fadebdb4d0d56ce034575e
Tyler Molamphy <tyler@molamphy.net>
2018-05-14 10:57:59 -0600

    New theme

--- vim/.vim/rake.sh
+++ vim/.vim/rake.sh
@@ -8,7 +8,7 @@ junegunn/fzf.git \
 nathanaelkane/vim-indent-guides.git \
 csexton/trailertrash.vim.git \
 vim-airline/vim-airline.git \
-morhetz/gruvbox.git"
+nanotech/jellybeans.vim.git "
 rm -rf ~/.vim/bundle/*
 for plugin in $plugins
 do git clone https://github.com/$plugin ~/.vim/bundle/`echo $plugin | cut -d / -f 2`
