c1b29ffa9de60b63760077a1dbb9444a7251f419
Tyler Molamphy <tyler@molamphy.net>
2018-05-25 12:19:23 -0600

    don't delete when done

--- vim/scripts/build-wiki.sh
+++ vim/scripts/build-wiki.sh
@@ -4,5 +4,6 @@ pushd ~/Dropbox/vimwiki_html
 open http://localhost:8001
 time python -m SimpleHTTPServer 8001
 popd
-echo "Cleaning up"
-rm -rf ~/Dropbox/vimwiki_html
+# echo "Cleaning up"
+# rm -rf ~/Dropbox/vimwiki_html
+echo Done
