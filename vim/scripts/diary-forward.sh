22b90a7a8b19373d126f5678c5ab5ba2917ee4a7
Tyler Molamphy <tyler@molamphy.net>
2018-06-12 16:05:05 -0600

    cant rely on symlinks on other systems

--- vim/scripts/diary-forward.sh
+++ vim/scripts/diary-forward.sh
@@ -1,10 +1,10 @@
-last=$(ls -t ~/vimwiki/diary/2*.md | head -1)
+last=$(ls -t ~/Dropbox/vimwiki/diary/2*.md | head -1)
 today=$(date +%Y-%m-%d)
-if [ -f ~/vimwiki/diary/$today.md ]; then
+if [ -f ~/Dropbox/vimwiki/diary/$today.md ]; then
 echo Nope
 exit 1
 else
-cat $last | grep -E '\[*\]|=' | grep -v "\[X\]" | sed "s/= 20.*-.*-.* =/= $(date +%Y-%m-%d) =/g" > ~/vimwiki/diary/$today.md
+cat $last | grep -E '\[*\]|=' | grep -v "\[X\]" | sed "s/= 20.*-.*-.* =/= $(date +%Y-%m-%d) =/g" > ~/Dropbox/vimwiki/diary/$today.md
 echo Done
 exit 0
 fi
