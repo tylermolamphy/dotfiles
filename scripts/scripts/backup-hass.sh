57cc84d3cfd170e139ac06b539a1d2c1c7d6c8cb
Tyler <t@Tylers-Air.lan>
2019-01-02 14:18:16 -0700

    backup homeassistant.io config

---
+++ scripts/scripts/backup-hass.sh
@@ -0,0 +1,7 @@
+cd ~/Hass
+rm -rfv config
+mkdir config
+cd config
+rsync -rv "root@10.10.10.42:/config/*" . -e"ssh -i /Users/t/.ssh/id_rsa_hass" --exclude "*.db"
+echo `date` >> ~/Hass/config/_timestamp
+echo Timestamped
