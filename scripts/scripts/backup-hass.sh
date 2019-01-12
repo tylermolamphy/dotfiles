94a65a968c7c936d34bb7b5dc3c6581e60fc961a
Tyler M <tyler@tylermolamphy.com>
2019-01-12 14:05:11 -0700

    no more scripts

--- scripts/scripts/backup-hass.sh
+++ scripts/scripts/backup-hass.sh
@@ -2,8 +2,6 @@ cd ~/Hass
 rm -rfv config
 mkdir config
 cd config
-rsync -rv "root@10.10.10.42:/config/*" . -e"ssh -i /Users/t/.ssh/id_rsa_hass" --exclude "*.db"
-mkdir ./scripts
-rsync -rv "root@10.10.10.42:/root/*" ./scripts -e"ssh -i /Users/t/.ssh/id_rsa_hass" --exclude "*.db"
+rsync -rv "root@10.10.10.42:/config/*" . -e"ssh -i ~/.ssh/id_rsa_hass" --exclude "*.db"
 echo `date` >> ~/Hass/config/_timestamp
 echo Timestamped
