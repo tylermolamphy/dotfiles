8f1d5830bb6f48e62932f3b7d628b0341a856d8d
Tyler M <tyler@tylermolamphy.com>
2019-01-16 09:40:15 -0700

    new seperate i3 config for desktop

---
+++ i3-desktop/scripts/install-powerline.sh
@@ -0,0 +1,3 @@
+sudo pacman -Syu powerline2 awesome-terminal-fonts powerline-fonts zsh-theme-powerlevel9k  --noconfirm
+sudo pip install powerline-status
+echo "source /usr/share/zsh-theme-powerlevel9k/powerlevel9k.zsh-theme" >> ~/.zsh/powerline.zsh
