e8181b8d51b3342f0afe5148d4b1a42312474202
Tyler M <tyler@tylermolamphy.com>
2019-02-05 16:38:26 -0700

    don't auto upgrade

--- scripts/scripts/install-base.sh
+++ scripts/scripts/install-base.sh
@@ -2,7 +2,6 @@
 cat /etc/os-release
 if [ -n "$(command -v apt)" ]; then
     sudo apt update
-    sudo apt dist-upgrade -y
     sudo apt install stow zsh tmux vim git keychain wget iftop mosh -y
     exit 0
 fi
