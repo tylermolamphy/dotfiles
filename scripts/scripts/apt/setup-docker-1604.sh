7d1bb72e8560e30a5a06c5c52664c0654f7cc35b
Tyler M <tyler@tylermolamphy.com>
2019-02-05 20:35:41 -0700

    clean up

--- scripts/scripts/apt/setup-docker-1604.sh
+++ scripts/scripts/apt/setup-docker-1604.sh
@@ -3,5 +3,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update
 apt install -y docker-ce
-systemctl status docker
+systemctl status docker --no-pager
 docker run hello-world
