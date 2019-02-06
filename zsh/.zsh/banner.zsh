e3f8ee9937051ddefa4d6fd7127b1e2799f4b6b5
Tyler <t@Tylers-MBP.local>
2019-02-06 10:43:16 -0700

    no more banner on mac

--- zsh/.zsh/banner.zsh
+++ zsh/.zsh/banner.zsh
@@ -1,4 +1,1 @@
-if [ -n "$(uname | grep Darwin)" ]; then
-echo $(whoami)@$(hostname | sed s/.local//g) running macos $(sw_vers -productVersion)
-fi
 if [ -d "/root/.dropbox-dist" ]; then echo Dropbox: $(/root/dropbox.py status) ; fi
