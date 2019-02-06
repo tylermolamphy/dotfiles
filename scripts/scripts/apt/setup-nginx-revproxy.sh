dc00454bc23173cddfe3cfbb0d980493217d99be
Tyler M <tyler@tylermolamphy.com>
2019-02-05 20:41:01 -0700

    quick and dirty nginx reverse proxy setup script

---
+++ scripts/scripts/apt/setup-nginx-revproxy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "Missing port number! What should I proxy on 80?"
+    exit 1
+fi
+apt install nginx-full -y
+systemctl start nginx --no-pager
+systemctl enable nginx
+systemctl status nginx --no-pager
+echo "server {
+listen 0.0.0.0:80 default_server;
+set \$upstream 127.0.0.1:$1;
+location / {
+proxy_pass_header Authorization;
+proxy_pass http://\$upstream;
+proxy_set_header Host \$host;
+proxy_set_header X-Real-IP \$remote_addr;
+proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+proxy_http_version 1.1;
+proxy_set_header Connection "";
+proxy_buffering off;
+client_max_body_size 0;
+proxy_read_timeout 36000s;
+proxy_redirect off;
+}
+}" > /etc/nginx/sites-enabled/default
+systemctl restart nginx --no-pager
