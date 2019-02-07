f67281073debaa4c904ce1c415f6e208c4fcb84f
Tyler M <tyler@tylermolamphy.com>
2019-02-06 17:51:53 -0700

    fix powerlevel9k on mac

--- zsh/.zsh/powerline.zsh
+++ zsh/.zsh/powerline.zsh
@@ -2,7 +2,7 @@ if [ -n "$(uname -a | grep Manjaro)" ]; then
 ~/powerlevel9k/powerlevel9k.zsh-theme
 fi
 if [ -n "$(uname | grep Darwin)" ]; then
-source /usr/share/zsh-theme-powerlevel9k/powerlevel9k.zsh-theme
+source /usr/local/Cellar/powerlevel9k/0.6.6/powerlevel9k.zsh-theme
 fi
 if [ -n "$(uname -a | grep Ubuntu)" ]; then
 source /usr/share/powerlevel9k/powerlevel9k.zsh-theme
