0cca27202e2429bef49b9d4ca73a3bd01cb0df47
Tyler M <tyler@tylermolamphy.com>
2019-02-08 22:21:48 -0700

    thelounge.chat instead

--- scripts/scripts/docker/thelounge.sh
+++ scripts/scripts/docker/thelounge.sh
@@ -1,3 +1,9 @@
-mkdir /data
-docker run -p 81:80 -p 444:443 -v /data:/data khlieng/dispatch &disown
-/root/dotfiles/scripts/scripts/apt/setup-nginx-revproxy.sh 444
+docker run --detach \
+	--name thelounge \
+	--publish 9000:9000 \
+	--volume ~/.thelounge:/var/opt/thelounge \
+	--restart always \
+	thelounge/thelounge:latest
+/root/dotfiles/scripts/scripts/apt/setup-nginx-revproxy.sh 9000
+/root/dotfiles/scripts/scripts/apt/setup-certbot-letsencrypt.sh
+echo Done
