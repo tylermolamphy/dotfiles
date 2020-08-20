9517d90004ead8931d180bae1ea27ce9e3338eeb
Tyler <tyler@git>
2020-08-19 22:06:58 -0600

    stop depcreating netstat and ifconfig!

--- scripts/scripts/install-base.sh
+++ scripts/scripts/install-base.sh
@@ -2,17 +2,17 @@
 cat /etc/os-release
 if [ -n "$(command -v apt)" ]; then
     sudo apt update
-    sudo apt install stow zsh tmux vim git keychain wget iftop netutils-tools mosh -y
+    sudo apt install stow zsh tmux vim git keychain wget iftop net-tools inetutils-tools mosh -y
     exit 0
 fi
 if [ -n "$(command -v dnf)" ]; then
     sudo dnf update -y
-    sudo dnf install stow zsh tmux vim keychain curl wget iftop inetutils-tools mosh -y
+    sudo dnf install stow zsh tmux vim keychain curl wget iftop net-tools inetutils-tools mosh -y
     exit 0
 fi
 if [ -n "$(command -v yum)" ]; then
     sudo yum update -y
-    sudo yum install stow zsh tmux vim keychain curl wget iftop inetutils-tools mosh -y
+    sudo yum install stow zsh tmux vim keychain curl wget iftop net-tools i6netutils-tools mosh -y
     exit 0
 fi
 if [ -n "$(command -v pacman)" ]; then
