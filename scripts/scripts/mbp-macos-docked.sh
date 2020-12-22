bb6b3f884a27c900831aab3ae703d6f801e0f6be
Tyler <tyler@git>
2020-12-22 15:57:23 -0700

    scripts for plugging/unplugging mac, launched w/ ControlPlane

---
+++ scripts/scripts/mbp-macos-docked.sh
@@ -0,0 +1,6 @@
+killall Amethyst
+defaults write com.apple.dock autohide -float 0
+killall Dock
+sleep 2
+open /Applications/Amethyst.app
+exit 0
---
+++ scripts/scripts/mbp-macos-undocked.sh
@@ -0,0 +1,4 @@
+killall Amethyst
+defaults write com.apple.dock autohide -float 1
+killall Dock
+exit 0
