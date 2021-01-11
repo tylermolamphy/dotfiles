049156ea26fbb1773c5258643103907a8cc39b47
Tyler <tyler@git>
2021-01-11 02:02:29 +0000

    small fixes

--- scripts/scripts/apt/setup-docker-2004.sh
+++ scripts/scripts/apt/setup-docker-2004.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 apt-get remove docker docker-engine docker.io containerd runc
-apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
 apt update
-apt install -y docker-ce docker-ce-cli containerd.io
+apt install -y docker.io
 systemctl start docker
 systemctl enable docker
 systemctl status docker --no-pager
