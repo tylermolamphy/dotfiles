6d5ff55ad933e59db1db50f2167fed1a5b029e2a
tylermolamphy <spam@tylermolamphy.com>
2021-01-17 12:42:10 -0700

    Update move.sh

--- move.sh
+++ move.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 pushd ~/dotfiles
-git remote remove origin
-git remote add origin git.molamphy.net:tm/dotfiles.git
+git pull
+for stowable in `find ~/dotfiles -type d -maxdepth 1 -mindepth 1 ! -name .git | xargs -n1 basename`
+  do stow -Dv $stowable
+done 
+popd
+pushd ~
+rm -r ~/dotfiles
+git clone git.molamphy.net:tm/dotfiles.git
+popd
+pushd ~/dotfiles
+for stowable in `find ~/dotfiles -type d -maxdepth 1 -mindepth 1 ! -name .git | xargs -n1 basename`
+  do stow -v $stowable
+done 
 popd
 exit 0
