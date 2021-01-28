714d125d040b2a23c9da82ca1008cfed900a72d7
Tyler <tyler@molamphy.net>
2021-01-27 18:33:43 -0700

    Add Amethyst to sleep scripts

--- scripts/scripts/mbp-macos-docked.sh
+++ scripts/scripts/mbp-macos-docked.sh
@@ -1,3 +1,7 @@
 defaults write com.apple.dock autohide -float 0
 killall Dock
+sleep 10
+killall Amethyst
+sleep 1
+open /Applications/Amethyst.app
 exit 0
--- scripts/scripts/mbp-macos-undocked.sh
+++ scripts/scripts/mbp-macos-undocked.sh
@@ -1,4 +1,7 @@
 defaults write com.apple.dock autohide -float 1
 killall Dock
 sleep 5
+killall Amethyst
+sleep 1
+open /Applications/Amethyst.app
 exit 0
