aaf1980acb7b1c7c276ba14ff808d529e6de7f5c
Tyler <tyler@molamphy.net>
2021-02-25 16:24:07 -0700

    Small tweaks

--- scripts/scripts/mbp-macos-docked.sh
+++ scripts/scripts/mbp-macos-docked.sh
@@ -2,7 +2,7 @@ killall BitBar
 killall Amethyst
 sleep 1
 /usr/local/bin/brightness .9
-defaults write com.apple.dock autohide -float 1
+defaults write com.apple.dock autohide -float C01
 killall Dock
 sleep 3
 open /Applications/Amethyst.app
