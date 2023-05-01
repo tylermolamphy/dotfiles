04d14dd8460433223e9c11606b32a7499bdb5335
Tyler <tyler@molamphy.net>
2023-05-01 11:41:01 -0600

    Change tmux to Dracula

--- tmux/.tmux/scripts/rake-tmux.sh
+++ tmux/.tmux/scripts/rake-tmux.sh
@@ -3,4 +3,5 @@ rm -rf ~/.tmux
 echo "Please wait, pulling dependencies for tmux"
 git clone -q https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 exit 0
