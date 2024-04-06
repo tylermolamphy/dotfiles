0c2c74c36aeb96e8f80a1264e11008555afa2282
Tyler <tyler@tylermolamphy.com>
2024-04-06 15:05:27 -0600

    Update path

--- scripts/scripts/clean-screenshots.sh
+++ scripts/scripts/clean-screenshots.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-mv ~/Desktop/Screen\ Shot* ~/Pictures/screenshots/
-mv ~/Desktop/Screen_Shot* ~/Pictures/screenshots/
-pre=$(ls -las ~/Pictures/screenshots | wc -l)
-/usr/local/sbin/tmpwatch --mtime --all 31d ~/Pictures/screenshots/
+mv ~/Desktop/Screen\ Shot* ~/Screenshots/
+mv ~/Desktop/Screen_Shot* ~/Screenshots/
+pre=$(ls -las ~/Screenshots | wc -l)
+/usr/local/sbin/tmpwatch --mtime --all 31d ~/Screenshots/
 echo Total: $pre
-echo Remaining: $(ls -las ~/Pictures/screenshots | wc -l)
+echo Remaining: $(ls -las ~/Screenshots | wc -l)
 exit 0
