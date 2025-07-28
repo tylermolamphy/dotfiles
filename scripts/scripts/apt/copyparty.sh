bf87e28a2497420caf7de3f273b9528cfd38f4a3
tylermolamphy <spam@tylermolamphy.com>
2025-07-28 15:36:49 -0600

    Update and rename install-copyparty.sh to copyparty.sh

--- scripts/scripts/apt/copyparty.sh
+++ scripts/scripts/apt/copyparty.sh
@@ -2,6 +2,6 @@
 apt install --no-install-recommends python3-pil pip ffmpeg
 python3 -m pip install --user -U copyparty
 ln -s `find / -name copyparty | grep bin | grep -v usr/bin` /usr/bin
-pushd /etc/systemd/system && wget https://github.com/9001/copyparty/raw/refs/heads/hovudstraum/contrib/package/arch/copyparty.service ; popd
-systemctl enable --now copyparty.service
+mkdir /var/lib/copyparty-jail
+/usr/bin/copyparty &disown
 exit 0
