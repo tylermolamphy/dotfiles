c119c19e01a596da7e59bd231493d51b66cbe133
Tyler <tyler@tylermolamphy.com>
2026-01-13 21:05:45 -0700

    Let's try tmux

--- scripts/scripts/apt/claude.sh
+++ scripts/scripts/apt/claude.sh
@@ -8,6 +8,9 @@ su - cclaude << 'EOF'
 whoami
 pwd
 curl -fsSL https://claude.ai/install.sh | bash
-echo ~/.local/bin/claude --dangerously-skip-permissions > ~/.zshrc
-~/.local/bin/claude --dangerously-skip-permissions
+echo "set -g status-style bg=colour99,fg=colour231" > ~/.tmux.conf
+echo "set -g mouse on" >> ~/.tmux.conf
+echo "set -g default-command '~/.local/bin/claude --dangerously-skip-permissions'" >> ~/.tmux.conf
+sleep 1
+tmux
 EOF
