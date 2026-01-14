a7e07d95879fc982b95b6d1908b2baff7421d6a6
Tyler <tyler@tylermolamphy.com>
2026-01-13 20:33:29 -0700

    Updates

--- scripts/scripts/apt/claude.sh
+++ scripts/scripts/apt/claude.sh
@@ -3,14 +3,11 @@
 if ! id -u cclaude &>/dev/null; then
   useradd -m -s /bin/bash cclaude
   echo "Created user 'cclaude'"
-	rm /etc/profile
 fi
 su - cclaude << 'EOF'
 whoami
 pwd
 curl -fsSL https://claude.ai/install.sh | bash
-echo ~/.local/bin/claude --dangerously-skip-permissions > ~/.bashrc
-rm ~/.profile ~/.bash_profile ~/.bash_login
-chsh -s /bin/bash
+echo ~/.local/bin/claude --dangerously-skip-permissions > ~/.zshrc
 ~/.local/bin/claude --dangerously-skip-permissions
 EOF
